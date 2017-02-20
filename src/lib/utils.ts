977484c856187030c08bcb2fe9aad24c61f9ba74
Eduardo Ronqui <edu.ronqui@gmail.com>
2017-02-20 01:04:46 -0300

    Fixed typo

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { Request } from 'express';
+
 const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
 const ARGUMENT_NAMES = /([^\s,]+)/g;
 
@@ -5,4 +7,10 @@ export function getFunctionParameters(func) {
   let fnStr = func.toString().replace(STRIP_COMMENTS, '');
   let result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
   return result || [];
+}
+
+export function getParameterValue(request: Request, key: string) {
+  if (key == 'request') return request;
+  if (key == 'body') return request.body;
+  return request.query[key] || request.params[key] || null;
 }
