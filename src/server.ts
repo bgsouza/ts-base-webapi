02eda225ad3fec8e0cd3c2ce1c53c9d28770b496
Eduardo Ronqui <edu.ronqui@gmail.com>
2017-02-20 01:04:32 -0300

    Added midllewares

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
+import "reflect-metadata";
+
 import * as express from 'express';
+import * as uuid from 'node-uuid';
+import * as morgan from 'morgan';
+import * as bodyParser from 'body-parser';
+
 import { BaseController } from './lib/base-controller';
 
 export class Server {
@@ -11,10 +17,23 @@ export class Server {
 
   constructor() {
     this.app = express();
+    this.registerMiddlewares();
     this.registerRoutes();
   }
 
   private registerRoutes() {
     BaseController.bindRoutes(this.app);
+  }
+
+  private registerMiddlewares() {
+    this.app.set('x-powered-by', false);
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }))
+    this.app.use(morgan(':method :url :status - :response-time ms'));
+    this.app.use((req: any, res, next) => {
+      req.id = uuid.v4();
+      res.setHeader('X-Reqquest-Id', req.id);
+      next();
+    });
   }
 }
